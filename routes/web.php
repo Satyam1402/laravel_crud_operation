26394fa7ea27fb44c74a3c7fd87749208101181a
Satyam1402 <satyamkprajapati2001@gmail.com>
2023-09-26 10:56:40 +0530

    files added

---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\mycontroller;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::view('/','insertRead');
+Route::post('insertData',[mycontroller::class,'insert']);
+Route::get('/',[mycontroller::class,'readdata']);
+// Route::view('update','updateview');
+Route::get('updatedelete',[mycontroller::class,'updateordelete']);
+Route::get('updatedata',[mycontroller::class,'update']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
